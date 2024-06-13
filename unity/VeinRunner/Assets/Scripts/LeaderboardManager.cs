ab36ac8db7e7b1832f968c484a1e3ab173aec53f
amani3639 <a.cermak@icloud.com>
2024-06-14 00:23:53 +0200

    final tasks
    - leaderboard (+calculation on death)
    - main menu input field

---
+++ unity/VeinRunner/Assets/Scripts/LeaderboardManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardManager : MonoBehaviour
+{
+
+    public GameObject boardTextObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardTextObject.GetComponent<TMPro.TextMeshProUGUI>().text = "";
+
+        for (int i = 0; i < 10; i++)
+        {
+            // load stored score data
+            string p_name = PlayerPrefs.GetString("score_name_" + i);
+            int p_score = PlayerPrefs.GetInt("score_points_" + i);
+            if (string.IsNullOrEmpty(p_name) && p_score == 0)
+            {
+                // empty slot
+                continue;
+            }
+            // print line on board
+            boardTextObject.GetComponent<TMPro.TextMeshProUGUI>().text += p_name + ": " + p_score + "\n";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- unity/VeinRunner/Assets/Scripts/MainMenuManager.cs
+++ unity/VeinRunner/Assets/Scripts/MainMenuManager.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI nameInputObject;
+
+
    public void StartGame(){
+        // start button clicked
+        this.SaveName();
         SceneManager.LoadScene(1); 
    }
    public void ExitGame(){
 		Application.Quit();
-	} 
+	}
+
+    public void SaveName()
+    {
+        // get name from text input field
+        string playerName = nameInputObject.text;
+        // store name for later use (PlayerController.RemoveHealth())
+        PlayerPrefs.SetString("PlayerName", playerName);
+    }
 }
